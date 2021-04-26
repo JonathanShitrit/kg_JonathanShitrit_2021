e21fb0a691b97f02f668cd14cbe87ddc74a63e46
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2021-04-26 17:48:56 -0400

    can now read cli input to function

---
+++ main.js
@@ -0,0 +1,28 @@
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// readline.question("Hit enter to see solution.", (arr) => {
+//     // console.log(...arr);
+//     console.log("done");
+//     readline.close();
+// })
+
+var arguments = process.argv
+
+// function add() {
+//     for (let i = 0; i < args.length; i++) {
+//         console.log(args[i]);
+//     }
+// }
+const phoneticInt = args => {
+    console.log("hello");
+}
+
+var result = phoneticInt(arguments)
+
+console.log(result)
+
+
+// phoneticInt();
